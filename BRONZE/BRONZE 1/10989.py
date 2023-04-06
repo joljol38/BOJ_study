c5d2402fde4d0a9f9fa66e5cba4cecbf167a3a61
Sinae Lee / 이시내 <joljol0108@gmail.com>
2023-04-06 18:27:28 +0900

    Add files via upload

---
+++ BRONZE/BRONZE 1/10989.py
@@ -0,0 +1,15 @@
+# 10989번 : 수 정렬하기 3
+
+import sys
+
+n = int(sys.stdin.readline())
+lst = [0] * 10001
+
+for _ in range(n):
+    lst[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if lst[i] != 0:
+        for j in range(lst[i]):
+            print(i)
+        
